c6d1f90f3f1651f674f60487daeb8a85357264f3
whyash5114@gmail.com <whyash5114@gmail.com>
2022-09-22 22:14:28 -0700

    added FileSelect screen

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 
+
 class MainScreen(Screen):
     pass
+
+
+class FileScreen(Screen):
+
+    def selectFile(self, selectedFile):
+        print(selectedFile)
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'MainScreen'
+
 
 class WindowManager(ScreenManager):
     pass
 
+
 class MyApp(App):
+
     def build(self):
         return Builder.load_file("styling.kv")
+
 
 MyApp().run()
