54efe9e8e29f00a62609295ab3c92e87c7f20ce8
whyash5114@gmail.com <whyash5114@gmail.com>
2022-09-22 21:24:54 -0700

    created helloWorld GUI just as a starting point

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+class MainScreen(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return Builder.load_file("styling.kv")
+
+MyApp().run()
