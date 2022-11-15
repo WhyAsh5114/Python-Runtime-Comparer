399398eafbb5f35621df312dcee0f033e9e46730
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-11-15 18:55:20 +0530

    Update postorder.py

---
+++ examples/TreeTraversal/postorder.py
@@ -0,0 +1,24 @@
+class Node:
+	def __init__(self, key):
+		self.left = None
+		self.right = None
+		self.val = key
+def printPostorder(root):
+
+	if root:
+
+		printPostorder(root.left)
+
+		printPostorder(root.right)
+
+		print(root.val),
+
+if __name__ == "__main__":
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print ("\nPostorder traversal of binary tree is")
+printPostorder(root)
