99503de19bc4c22ff937fff68f5d67a2159614f1
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-11-28 22:59:27 +0530

    added a start button

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ class MainScreen(Screen):
     def choose_file(self, choosing):
         global selecting_file
         selecting_file = choosing
+    
+    def update_start_button_text(self):
+        if self.ids['input_file_text_input'].text != '' and self.ids['output_file_text_input'].text != '' and self.ids['runner_file_text_input'].text != '':
+            self.ids['start_button'].text = 'Start plot'
+        else:
+            self.ids['start_button'].text = 'Select all 3 files to continue'
 
 
 class FileScreen(Screen):
@@ -17,12 +23,17 @@ class FileScreen(Screen):
         global selecting_file
         self.manager.transition.direction = 'right'
         self.manager.current = 'MainScreen'
+
+        selectedFile = len(selectedFile) != 0 and selectedFile[0] or ''
+        main_screen = self.manager.get_screen('MainScreen')
         if selecting_file == 'input':
-            self.manager.get_screen('MainScreen').ids['input_file_text_input'].text = str(selectedFile[0])
+            main_screen.ids['input_file_text_input'].text = selectedFile
         elif selecting_file == 'output':
-            self.manager.get_screen('MainScreen').ids['output_file_text_input'].text = str(selectedFile[0])
+            main_screen.ids['output_file_text_input'].text = selectedFile
         elif selecting_file == 'runner':
-            self.manager.get_screen('MainScreen').ids['runner_file_text_input'].text = str(selectedFile[0])
+            main_screen.ids['runner_file_text_input'].text = selectedFile
+        
+        main_screen.update_start_button_text()
 
 
 class WindowManager(ScreenManager):
