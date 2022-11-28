58f2958bb43962785cd3c778698224cc2514b38f
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-11-28 22:39:50 +0530

    added input and output file choosers

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 
+selecting_file = None
 
 class MainScreen(Screen):
-    pass
+    
+    def choose_file(self, choosing):
+        global selecting_file
+        selecting_file = choosing
 
 
 class FileScreen(Screen):
 
     def selectFile(self, selectedFile):
-        print(selectedFile)
+        global selecting_file
         self.manager.transition.direction = 'right'
         self.manager.current = 'MainScreen'
+        if selecting_file == 'input':
+            self.manager.get_screen('MainScreen').ids['input_file_text_input'].text = str(selectedFile[0])
+        elif selecting_file == 'output':
+            self.manager.get_screen('MainScreen').ids['output_file_text_input'].text = str(selectedFile[0])
+        elif selecting_file == 'runner':
+            self.manager.get_screen('MainScreen').ids['runner_file_text_input'].text = str(selectedFile[0])
 
 
 class WindowManager(ScreenManager):
