848c7ad75296b4693032c5f7862853e424e8c71a
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-12-02 01:47:25 +0530

    idk

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
@@ -15,6 +16,30 @@ class MainScreen(Screen):
             self.ids['start_button'].text = 'Start plot'
         else:
             self.ids['start_button'].text = 'Select all 2 files to continue'
+    
+    def start_plot(self):
+        input_file = self.ids['input_file_text_input'].text
+        program_file = self.ids['runner_file_text_input'].text
+
+        f = open(input_file, 'r')
+        input_data = f.readlines()
+        f.close()
+        
+        all_times = []
+        for i, test_case in enumerate(input_data, 1):
+            f = open('input.txt', 'w+')
+            f.write(test_case)
+            f.close()
+            os.system("python " + program_file)
+            f = open('output.txt', 'r')
+            time_taken = f.read()
+            f.close()
+            all_times.append(time_taken)
+            print(i, "done")
+        f = open("plot.txt", 'w+')
+        f.write(str(all_times))
+        f.close()
+        print(all_times)
 
 
 class FileScreen(Screen):
