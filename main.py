fe2c2cdabf4afb3364738616121108087fdcba7f
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-12-05 21:23:52 +0530

    added graph screen

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import os
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+from matplotlib import pyplot as plt
+from kivy.garden.matplotlib import FigureCanvasKivyAgg
+from sympy import Eq, solve
+from sympy.abc import x, y, z
 
 selecting_file = None
+runner_filename = ''
 
 class MainScreen(Screen):
     
@@ -39,7 +44,12 @@ class MainScreen(Screen):
         f = open("plot.txt", 'w+')
         f.write(str(all_times))
         f.close()
-        print(all_times)
+
+        global runner_filename
+        runner_filename = os.path.basename(program_file)
+        
+        self.manager.current = 'GraphScreen'
+        self.manager.transition.direction = 'left'
 
 
 class FileScreen(Screen):
@@ -57,6 +67,35 @@ class FileScreen(Screen):
             main_screen.ids['runner_file_text_input'].text = selectedFile
         
         main_screen.update_start_button_text()
+
+
+class GraphScreen(Screen):
+    
+    def on_pre_enter(self, *args):
+        f = open('plot.txt')
+        data = f.read()
+        f.close()
+        plt.close()
+        plt.figure().clear()
+
+        data = data.strip('][').split(', ')
+        data = [float(x.replace("'", '')) for x in data]
+        xdata = range(len(data))
+
+        sol = solve([Eq(len(data)*x + sum(xdata)*y + sum(map(lambda x: x*x, xdata))*z, sum(data)),
+                    Eq(sum(xdata)*x + sum(map(lambda x: x*x, xdata))*y + sum(map(lambda x: x**3, xdata))*z, sum(map(lambda x, y: x*y, xdata, data))),
+                    Eq(sum(map(lambda x: x*x, xdata))*x + sum(map(lambda x: x**3, xdata))*y + sum(map(lambda x: x**4, xdata))*z, sum(map(lambda x, y: x*x*y, xdata, data)))])
+        plt.plot(data)
+
+        ydata = []
+        for i in range(len(data)):
+            ydata.append(sol[x] + sol[y]*i + sol[z]*i*i)
+        plt.plot(ydata)
+
+        global runner_filename
+        plt.title(runner_filename)
+
+        self.ids['main_layout'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
 
 
 class WindowManager(ScreenManager):
