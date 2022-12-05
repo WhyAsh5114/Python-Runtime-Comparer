f17912d85e7840b81bae58786704a01fc633e8fd
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-12-05 21:26:34 +0530

    better input generation

--- examples/ArraySorting/inputGenerator.py
+++ examples/ArraySorting/inputGenerator.py
@@ -2,9 +2,9 @@ from random import randint
 from tqdm import tqdm
 
 data = ""
-for i in tqdm(range(1, 501)):
-    for i in range(20 * i):
-        data += str(randint(0, 1000000)) + " "
+for i in tqdm(range(1, 11)):
+    for i in range(100000 * i):
+        data += str(randint(0, 1000)) + " "
     data += "\n"
 
 f = open('allInputs.txt', 'w+')
